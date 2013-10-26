86cd6853bd9729952aa0f4db0f2f1bac0cf3ad30
Rick <rick.bolton@gmail.com>
2013-10-26 17:13:45 +0100

    Created angular dashboard controller

---
+++ Heisenberg/App/Dashboard/Dashboard.js
@@ -0,0 +1,24 @@
+﻿var heisenbergApp = angular.module('heisenbergApp', []);
+
+heisenbergApp.controller('DashboardCtrl', function DashboardCtrl($scope) {
+    $scope.teamMembers = [
+      {
+          'username': 'MrCochese',
+          'name': 'Rick'
+      },
+      {
+          'username': 'GraanJonlo',
+          'name': 'Andy'
+      },
+      {
+          'username': 'sichiu',
+          'name': 'Si'
+      },
+      {
+          'username': 'lhbt',
+          'name': 'Laurent'
+      }
+    ];
+
+    $scope.orderProp = 'name';
+});
