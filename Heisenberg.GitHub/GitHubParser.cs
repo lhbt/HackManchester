9df5078b34cbaf2db8500dd59d6e58e395311ab9
lhbt <laurent.humbert8@gmail.com>
2013-10-26 19:41:06 +0100

    creation of the generic SourceControlService

--- Heisenberg.GitHub/GitHubParser.cs
+++ Heisenberg.GitHub/GitHubParser.cs
@@ -1,11 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using GitSharp;
 using GitSharp.Core.RevPlot;
+using GitSharp.Core.Util;
+using Heisenberg.SourceControlService;
 
 namespace Heisenberg.GitHub
 {
-    public class GitHubParser : GitHubApiWrapper
+    public class GitHubParser : ISourceControlParser
     {
         public GitHubParser(string repoPath)
         {
@@ -14,19 +17,63 @@ namespace Heisenberg.GitHub
 
         public Repository Repository { get; private set; }
 
-        public List<GitSharp.Core.Commit> GetCommits()
+        public IEnumerable<string> GetLanguagesUsed()
         {
-            var mRevwalk = new PlotWalk(Repository);
-            mRevwalk.markStart(((GitSharp.Core.Repository)Repository).getAllRefsByPeeledObjectId().Keys.Select(mRevwalk.parseCommit));
+            throw new System.NotImplementedException();
+        }
 
-            return mRevwalk.Select(commit => commit.AsCommit(mRevwalk)).Select(dummy => dummy).ToList();
+        public IEnumerable<string> GetFilesList()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int GetNumberOfCommitsWithKeywordInComment(string keyword)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int GetNumberOfCommitsInTheLastHour()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int GetAmountOfLinesOfCode()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int GetAmountOfMinutesSinceLastCommit()
+        {
+            throw new System.NotImplementedException();
         }
 
         public List<string> GetFiles()
         {
             var files = new List<string>();
 
+
+
             return files;
+        }
+
+        public List<CommitWrapper> GetCommits()
+        {
+            var revWalk = new PlotWalk(Repository);
+            revWalk.markStart(((GitSharp.Core.Repository)Repository).getAllRefsByPeeledObjectId().Keys.Select(revWalk.parseCommit));
+
+            var commits = new List<CommitWrapper>();
+            foreach (var commit in revWalk)
+            {
+                var tmp = commit.AsCommit(revWalk);
+                
+                commits.Add(new CommitWrapper
+                {
+                    Author = commit.getAuthorIdent().EmailAddress,
+                    Comment = commit.getFullMessage(),
+                    TimeCommited = tmp.Author.When.MillisToUtcDateTime()
+                });
+            }
+            return commits;
         }
     }
 }
