8138460ee7fc54313476faf88a792da6e92a10af
Simon Chiu <si@sichiu.co.uk>
2013-10-26 20:05:25 +0100

    Renamed Twitter controller

---
+++ Heisenberg/Controllers/TwitterHashtagController.cs
@@ -0,0 +1,15 @@
+﻿using Heisenberg.Twitter;
+using System.Web.Http;
+
+namespace Heisenberg.Controllers
+{
+    public class TwitterHashtagController : ApiController
+    {
+        public TwitterStatusResponse Get()
+        {
+            var twitterApiWrapper = new TwitterApiWrapper();
+
+            return twitterApiWrapper.QueryHashtag("#hackmanchester");
+        }
+    }
+}
