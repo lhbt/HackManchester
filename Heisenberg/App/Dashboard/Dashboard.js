af3e11f1e218c6f4d7e09bcfcda0a4b9e7d2da52
Rick <rick.bolton@gmail.com>
2013-10-26 19:18:59 +0100

    angular routing added

--- Heisenberg/App/Dashboard/Dashboard.js
+++ Heisenberg/App/Dashboard/Dashboard.js
@@ -1,28 +1,7 @@
-﻿var heisenbergApp = angular.module('heisenbergApp', []);
-
-heisenbergApp.controller('DashboardCtrl', function DashboardCtrl($scope, $http) {
+﻿heisenbergControllers.controller('DashboardCtrl', function DashboardCtrl($scope, $http) {
     $http.get('App/Dashboard/TeamMembers.json').success(function (data) {
         $scope.teamMembers = data;
     });
-
-    /*$scope.teamMembers = [
-      {
-          'username': 'MrCochese',
-          'name': 'Rick'
-      },
-      {
-          'username': 'GraanJonlo',
-          'name': 'Andy'
-      },
-      {
-          'username': 'sichiu',
-          'name': 'Si'
-      },
-      {
-          'username': 'lhbt',
-          'name': 'Laurent'
-      }
-    ];*/
 
     $scope.orderProp = 'name';
 });
