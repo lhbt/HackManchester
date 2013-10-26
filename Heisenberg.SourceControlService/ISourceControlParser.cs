19072a4585f248c7ca8ade3031ba2d1b786f3f3c
lhbt <laurent.humbert8@gmail.com>
2013-10-26 19:43:33 +0100

    fix merge

---
+++ Heisenberg.SourceControlService/ISourceControlParser.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Heisenberg.SourceControlService
+{
+    public interface ISourceControlParser
+    {
+        List<CommitWrapper> GetCommits();
+        IEnumerable<string> GetLanguagesUsed();
+        IEnumerable<string> GetFilesList();
+        int GetNumberOfCommitsWithKeywordInComment(string keyword);
+        int GetNumberOfCommitsInTheLastHour();
+        int GetAmountOfLinesOfCode();
+        int GetAmountOfMinutesSinceLastCommit();
+    }
+}
