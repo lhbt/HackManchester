8e2b1bcf26c888fcad1665a55905943b19d6a15e
Simon Chiu <si@sichiu.co.uk>
2013-10-27 03:07:03 +0000

    Removed TwitterFilters from service.js

--- Heisenberg/App/Services.js
+++ Heisenberg/App/Services.js
@@ -9,7 +9,7 @@ heisenbergServices.factory('TeamMember', ['$resource',
 
 heisenbergServices.factory('Tweets', ['$resource',
   function ($resource) {
-      return $resource('api/twitterhashtag/get', {}, {
+      return $resource('api/twitterhashtag', {}, {
           query: { method: 'GET', params: {}, isArray: true }
       });
   }]);
