e4af7b6449ecd1af421b74a441c99f133aaf5e92
lhbt <laurent.humbert8@gmail.com>
2013-10-27 12:34:13 +0000

    merge

--- Heisenberg/App/Dashboard/Dashboard.js
+++ Heisenberg/App/Dashboard/Dashboard.js
@@ -1,28 +1,11 @@
-﻿heisenbergControllers.controller('DashboardCtrl', ['$scope', 'TeamMember', 'Tweets', 'Commits', 'CommitLanguages', 'BytesOfCode', 'MinutesSinceCommit', 'MostRecentBuildResults', function DashboardCtrl($scope, TeamMember, Tweets, commits, CommitLanguages, bytesofCode, minutesSinceCommit, mostRecentBuildResults) {
-    $scope.teamMembers = TeamMember.query();
-    $scope.orderProp = 'name';
-
-    $scope.commits = commits.query();
-<<<<<<< HEAD
-    $scope.commitlanguages = CommitLanguages.query();
-    $scope.bytesofcode = bytesofCode.query();
-    $scope.minutessincecommit = minutesSinceCommit.query();
-
-=======
-    $scope.commitlanguages = CommitLanguages.query;
-    $scope.bytesofcode = bytesofCode.query;
-    $scope.minutessincecommit = minutesSinceCommit.query;
-    $scope.mostrecentbuildresults = mostRecentBuildResults.query;
->>>>>>> 9ed423a7f74787ae013b0ed21ce52aa622fd2c61
-    //$scope.onTimeout = function() {
-        //$scope.teamMembers = TeamMember.query();
-        //$scope.tweets = Tweets.query();
-        //mytimeout = $timeout($scope.onTimeout, 20000);
-    //};
-    //var mytimeout = $timeout($scope.onTimeout, 20000);
-
-    $scope.update = function() {
-        $scope.teamMembers = TeamMember.query();
-        $scope.tweets = Tweets.query();
-    };
-}]);
+﻿heisenbergControllers.controller('DashboardCtrl',
+    ['$scope', 'Commits', 'CommitLanguages', 'BytesOfCode', 'MinutesSinceCommit', 'MostRecentBuildResults',
+        function DashboardCtrl($scope, commits, commitLanguages, bytesofCode, minutesSinceCommit, mostRecentBuildResults)
+        {
+            $scope.commits = commits.query();
+            $scope.commitlanguages = commitLanguages.query();
+            $scope.bytesofcode = bytesofCode.query();
+            $scope.minutessincecommit = minutesSinceCommit.query();
+        }
+    ]
+);
