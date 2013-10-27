3ca19535edbbcd4cd75b382b69c9b77df0a21221
lhbt <laurent.humbert8@gmail.com>
2013-10-27 12:42:08 +0000

    fix merge

--- Heisenberg/App/Dashboard/Dashboard.js
+++ Heisenberg/App/Dashboard/Dashboard.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿heisenbergControllers.controller('DashboardCtrl',
+﻿﻿heisenbergControllers.controller('DashboardCtrl',
     ['$scope', 'Commits', 'CommitLanguages', 'BytesOfCode', 'MinutesSinceCommit', 'MostRecentBuildResults',
         function DashboardCtrl($scope, commits, commitLanguages, bytesofCode, minutesSinceCommit, mostRecentBuildResults)
         {
@@ -7,31 +6,7 @@
             $scope.commitlanguages = commitLanguages.query();
             $scope.bytesofcode = bytesofCode.query();
             $scope.minutessincecommit = minutesSinceCommit.query();
+            $scope.mostrecentbuildresults = mostRecentBuildResults.query();
         }
     ]
 );
-=======
-﻿heisenbergControllers.controller('DashboardCtrl', ['$scope', 'TeamMember', 'Tweets', 'Commits', 'CommitLanguages', 'BytesOfCode', 'MinutesSinceCommit', 'MostRecentBuildResults', function DashboardCtrl($scope, TeamMember, Tweets, commits, CommitLanguages, bytesofCode, minutesSinceCommit, mostRecentBuildResults) {
-    $scope.teamMembers = TeamMember.query();
-    $scope.orderProp = 'name';
-
-    $scope.commits = commits.query();
-
-    $scope.commitlanguages = CommitLanguages.query;
-    $scope.bytesofcode = bytesofCode.query;
-    $scope.minutessincecommit = minutesSinceCommit.query;
-    $scope.mostrecentbuildresults = mostRecentBuildResults.query;
-
-    //$scope.onTimeout = function() {
-        //$scope.teamMembers = TeamMember.query();
-        //$scope.tweets = Tweets.query();
-        //mytimeout = $timeout($scope.onTimeout, 20000);
-    //};
-    //var mytimeout = $timeout($scope.onTimeout, 20000);
-
-    $scope.update = function() {
-        $scope.teamMembers = TeamMember.query();
-        $scope.tweets = Tweets.query();
-    };
-}]);
->>>>>>> a910fe75f918e79ce9d4e3f11f7f1eac9ab2cf9d
